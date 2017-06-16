1bd3f04dd78d89904ed0b200f8fac7cd0137646e
Harunobu Daikoku <daikoku@hpcs.cs.tsukuba.ac.jp>
2017-06-16 10:01:10 +0900

    implement Scatter/Gather bandwidth test

--- examples/sg_bandwidth_client.cc
+++ examples/sg_bandwidth_client.cc
@@ -2,54 +2,51 @@
 // Created by Harunobu Daikoku on 2017/06/15.
 //
 
-#include <chrono>
 #include <iostream>
 #include <rnetlib/rnetlib.h>
 
 using namespace rnetlib;
 
-void do_pingpong(const Channel &channel, size_t msg_len) {
-  std::unique_ptr<char[]> msg(new char[msg_len]);
-  std::memset(msg.get(), 'a', msg_len);
-
-  auto local_mem = channel.RegisterMemory(msg.get(), msg_len, MR_LOCAL_WRITE);
-
-  auto beg = std::chrono::steady_clock::now();
-  for (int i = 0; i < 5000; i++) {
-    if (channel.Send(*local_mem) != msg_len) {
-      std::cerr << "ERROR: send" << std::endl;
-      return;
-    }
-  }
-  auto end = std::chrono::steady_clock::now();
-
-  auto dur = std::chrono::duration_cast<std::chrono::microseconds>(end - beg).count();
-  auto bw = (40. * msg_len) / dur;
-
-  std::cout << msg_len << "\t" << bw << std::endl;
-}
-
 int main(int argc, const char **argv) {
-  if (argc != 3) {
-    std::cerr << "Usage: " << argv[0] << " [addr] [port]" << std::endl;
+  if (argc != 5) {
+    std::cerr << "Usage: " << argv[0] << " [addr] [port] [msg_size] [num_iters]" << std::endl;
     return 1;
   }
 
+  size_t msg_size = std::stoi(argv[3]);
+  int num_iters = std::stoi(argv[4]);
+
   // FIXME: handle errors
-  auto loop = RNetLib::NewEventLoop(RNetLib::Mode::SOCKET);
   auto client = RNetLib::NewClient(argv[1], static_cast<uint16_t>(std::stoul(argv[2])), RNetLib::Mode::SOCKET);
-  auto future_channel = client->Connect(*loop);
+  auto channel = client->Connect();
 
-  loop->Run(30 * 1000);
-
-  auto channel = future_channel.get();
-  channel->SetNonBlocking(false);
-
-  size_t max_bytes = (1 << 27);
-  std::cout << "Length[Bytes]" << "\t" << "Bandwidth[Gbit/s]" << std::endl;
-  for (size_t i = 1; i <= max_bytes; i *= 2) {
-    do_pingpong(*channel, i);
+  std::vector<std::unique_ptr<char[]>> blks;
+  std::vector<std::unique_ptr<LocalMemoryRegion>> mrs;
+  for (int i = 0; i < num_iters; i++) {
+    std::unique_ptr<char[]> blk(new char[msg_size]);
+    mrs.emplace_back(channel->RegisterMemory(blk.get(), msg_size, MR_LOCAL_READ | MR_LOCAL_WRITE));
+    blks.emplace_back(std::move(blk));
   }
+
+  auto beg = std::chrono::steady_clock::now();
+  for (int i = 0; i < num_iters; i++) {
+    channel->Send(blks[i].get(), msg_size);
+    channel->Recv(blks[i].get(), msg_size);
+  }
+  auto end = std::chrono::steady_clock::now();
+  auto dur = std::chrono::duration_cast<std::chrono::milliseconds>(end - beg).count();
+
+  std::cout << "send by iter.: " << dur << " [msecs], Bandwidth: "
+            << (msg_size * num_iters * 16) / (dur * 1000000) << " [Gbit/s]" << std::endl;
+
+  beg = std::chrono::steady_clock::now();
+  channel->SendSG(mrs);
+  channel->RecvSG(mrs);
+  end = std::chrono::steady_clock::now();
+  dur = std::chrono::duration_cast<std::chrono::milliseconds>(end - beg).count();
+
+  std::cout << "send by iovec: " << dur << " [msecs], Bandwidth: "
+            << (msg_size * num_iters * 16) / (dur * 1000000) << " [Gbit/s]" << std::endl;
 
   return 0;
 }
--- examples/sg_bandwidth_server.cc
+++ examples/sg_bandwidth_server.cc
@@ -2,3 +2,53 @@
 // Created by Harunobu Daikoku on 2017/06/15.
 //
 
+#include <chrono>
+#include <iostream>
+#include <rnetlib/rnetlib.h>
+
+using namespace rnetlib;
+
+int main(int argc, const char **argv) {
+  if (argc != 4) {
+    std::cerr << "Usage: " << argv[0] << " [port] [msg_size] [num_iters]" << std::endl;
+    return 1;
+  }
+
+  size_t msg_size = std::stoi(argv[2]);
+  int num_iters = std::stoi(argv[3]);
+
+  // FIXME: handle errors
+  auto server = RNetLib::NewServer("0.0.0.0", static_cast<uint16_t>(std::stoul(argv[1])), RNetLib::Mode::SOCKET);
+  server->Listen();
+  auto channel = server->Accept();
+
+  std::vector<std::unique_ptr<char[]>> blks;
+  std::vector<std::unique_ptr<LocalMemoryRegion>> mrs;
+  for (int i = 0; i < num_iters; i++) {
+    std::unique_ptr<char[]> blk(new char[msg_size]);
+    mrs.emplace_back(channel->RegisterMemory(blk.get(), msg_size, MR_LOCAL_READ | MR_LOCAL_WRITE));
+    blks.emplace_back(std::move(blk));
+  }
+
+  auto beg = std::chrono::steady_clock::now();
+  for (int i = 0; i < num_iters; i++) {
+    channel->Recv(blks[i].get(), msg_size);
+    channel->Send(blks[i].get(), msg_size);
+  }
+  auto end = std::chrono::steady_clock::now();
+  auto dur = std::chrono::duration_cast<std::chrono::milliseconds>(end - beg).count();
+
+  std::cout << "send by iter.: " << dur << " [msecs], Bandwidth: "
+            << (msg_size * num_iters * 16) / (dur * 1000000) << " [Gbit/s]" << std::endl;
+
+  beg = std::chrono::steady_clock::now();
+  channel->RecvSG(mrs);
+  channel->SendSG(mrs);
+  end = std::chrono::steady_clock::now();
+  dur = std::chrono::duration_cast<std::chrono::milliseconds>(end - beg).count();
+
+  std::cout << "send by iovec: " << dur << " [msecs], Bandwidth: "
+            << (msg_size * num_iters * 16) / (dur * 1000000) << " [Gbit/s]" << std::endl;
+
+  return 0;
+}
