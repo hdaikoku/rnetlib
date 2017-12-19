a29801fe98bea180e9ae540d1305315893eb4551
Harunobu Daikoku <daikoku@hpcs.cs.tsukuba.ac.jp>
2017-12-19 10:39:00 +0900

    post recv request on-demand

--- include/rnetlib/ofi/ofi_channel.h
+++ include/rnetlib/ofi/ofi_channel.h
@@ -14,10 +14,9 @@ namespace ofi {
 class OFIChannel : public Channel {
  public:
   OFIChannel(OFIEndpoint &ep, fi_addr_t peer_addr, uint64_t peer_desc, uint64_t src_tag)
-      : ep_(ep), peer_addr_(peer_addr), peer_desc_(peer_desc), src_tag_(src_tag), dst_tag_(0), recv_buf_(*this) {
+      : ep_(ep), peer_addr_(peer_addr), peer_desc_(peer_desc), src_tag_(src_tag), dst_tag_(0) {
     std::memset(&tx_req_, 0, sizeof(tx_req_));
     std::memset(&rx_req_, 0, sizeof(rx_req_));
-    ep_.PostRecv(recv_buf_.GetAddr(), EAGER_THRESHOLD, recv_buf_.GetLKey(), src_tag_, &rx_req_);
   }
 
   ~OFIChannel() override { ep_.RemoveAddr(&peer_addr_); }
@@ -55,27 +54,9 @@ class OFIChannel : public Channel {
       if (len == 0) {
         continue;
       }
-      auto addr = lmr[i]->GetAddr();
-      auto lkey = lmr[i]->GetLKey();
 
-      if (sent_len == 0) {
-        // this is the very first part of the transfer, send it to the pre-allocated ReceiveBuffer.
-        auto sending_len = (len > EAGER_THRESHOLD) ? EAGER_THRESHOLD : len;
-        ep_.PostSend(addr, sending_len, lkey, peer_addr_, dst_tag_, &tx_req_);
-        if (ep_.PollTxCQ(tx_req_.req, &tx_req_) != 1) {
-          return 0;
-        }
-        sent_len += sending_len;
-
-        len -= sending_len;
-        if (len == 0) {
-          continue;
-        }
-        addr = static_cast<char *>(addr) + sending_len;
-      }
-
-      iov.push_back({addr, len});
-      desc.push_back(lkey);
+      iov.push_back({lmr[i]->GetAddr(), len});
+      desc.push_back(lmr[i]->GetLKey());
       sent_len += len;
     }
 
@@ -88,10 +69,8 @@ class OFIChannel : public Channel {
   }
 
   size_t RecvV(const LocalMemoryRegion::ptr *lmr, size_t lmrcnt) override {
-    assert(rx_req_.req == 1);
+    assert(rx_req_.req == 0);
     size_t recvd_len = 0;
-    void *head_addr = nullptr;
-    size_t head_len = 0;
     std::vector<struct iovec> iov;
     std::vector<void *> desc;
     iov.reserve(lmrcnt);
@@ -102,45 +81,16 @@ class OFIChannel : public Channel {
       if (len == 0) {
         continue;
       }
-      auto addr = lmr[i]->GetAddr();
-      auto lkey = lmr[i]->GetLKey();
 
-      if (recvd_len == 0) {
-        // this is the very first part of the transfer, receive it with the pre-allocated ReceiveBuffer.
-        head_addr = addr;
-        if (len > EAGER_THRESHOLD) {
-          // the first SGE won't fit in the the pre-allocated ReceiveBuffer
-          head_len = EAGER_THRESHOLD;
-          len -= EAGER_THRESHOLD;
-          addr = static_cast<char *>(addr) + EAGER_THRESHOLD;
-          recvd_len += EAGER_THRESHOLD;
-        } else {
-          head_len = len;
-          recvd_len += len;
-          continue;
-        }
-      }
-
-      iov.push_back({addr, len});
-      desc.push_back(lkey);
+      iov.push_back({lmr[i]->GetAddr(), len});
+      desc.push_back(lmr[i]->GetLKey());
       recvd_len += len;
     }
 
     ep_.PostRecv(iov.data(), desc.data(), iov.size(), src_tag_, &rx_req_);
 
-    // refill a recv request for a header.
-    ep_.PostRecv(recv_buf_.GetAddr(), EAGER_THRESHOLD, recv_buf_.GetLKey(), src_tag_, &rx_req_);
-
-    ep_.PollRxCQ(rx_req_.req - 1, &rx_req_);
-    if (rx_req_.req != 1) {
-      // error
-      return 0;
-    }
-
-    // copy the received header part to the user buffer.
-    recv_buf_.Read(head_addr, head_len);
-
-    return recvd_len;
+    ep_.PollRxCQ(rx_req_.req, &rx_req_);
+    return (rx_req_.req == 0) ? recvd_len : 0;
   }
 
   size_t ISendV(const LocalMemoryRegion::ptr *lmr, size_t lmrcnt, const EventLoop::ptr &evloop) override {
@@ -268,8 +218,6 @@ class OFIChannel : public Channel {
   uint64_t dst_tag_;
   struct ofi_req tx_req_;
   struct ofi_req rx_req_;
-  // pre-allocated buffer for eager send/recv
-  EagerBuffer recv_buf_;
 };
 
 } // namespace ofi
